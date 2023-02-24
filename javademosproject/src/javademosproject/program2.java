ed28871f6267f690a6765fc5df7c9c68d7ecefff
S J PremKumar Nayaka <spremkumarna@BLR.ALLEGISINDIA.COM>
2023-02-24 13:06:53 +0530

    testing

--- javademosproject/src/javademosproject/program2.java
+++ javademosproject/src/javademosproject/program2.java
@@ -1,5 +1,22 @@
 package javademosproject;
 
 public class program2 {
+	int dem3;
+	
+	public static void main(String[] args) {
+		bike b1 =new bike();
+	}
 
+}
+class vehicle{
+	vehicle(){
+		System.out.println("vehicle");
+	}
+	
+
+}
+class bike extends vehicle{
+	bike(){
+		System.out.println("bike");
+	}
 }

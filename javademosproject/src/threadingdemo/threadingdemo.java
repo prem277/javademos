1e3f3ae9bec1336bceaaf13ca8f0ed3d2d4a8be5
S J PremKumar Nayaka <spremkumarna@BLR.ALLEGISINDIA.COM>
2023-03-03 09:20:04 +0530

    testing'

--- javademosproject/src/threadingdemo/threadingdemo.java
+++ javademosproject/src/threadingdemo/threadingdemo.java
@@ -2,23 +2,34 @@ package threadingdemo;
 
 public class threadingdemo {
 	public static void main(String[] args) {
-		singlethread st=new singlethread();
-		st.print();
 		multithread mt=new multithread();
 		mt.start();
+		singlethread st=new singlethread();
+		st.print();
+		
 	}
 }
 class multithread extends Thread{
 	public void run(){
-		for(int i=0;i<10;i++)
-		System.out.println("hello from multiple thread\n");	
-	
+		for(int i=0;i<100;i++) {
+		System.out.println("hello from multiple thread\n");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		}
 	
 }
 }
 class singlethread {
 	void print(){
-		for(int i=0;i<10;i++)
+		for(int i=0;i<100;i++) 
+		{
 		System.out.println("hello from single thread\n");
-	}
+		
+	}}
+		
 }
